a4f1360f89bd40e9b69443baa56e0e2b66fc8da9
Sergio <serchisob@gmail.com>
2018-09-26 11:58:16 +0200

    MenuController

---
+++ ProyectoAqua/Assets/Scripts/MenuController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+
+	public GameObject deathMenu;
+	public GameObject pauseMenu;
+	public static MenuController instance;
+
+	 // Use this for initialization
+    void Start () {
+		if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+	}
+	
+	public void onClickRetry(){
+		
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+	}
+
+	public void onClickTitleScreen(){
+		
+		SceneManager.LoadScene("TitleScreen", LoadSceneMode.Single);
+	}
+
+	public void showDeathMenu(){
+		Time.timeScale = 0.0000001f;
+		deathMenu.SetActive(true);
+	}
+
+	public void showPauseMenu(){
+		Time.timeScale = 0.0000001f;
+		pauseMenu.SetActive(true);
+	}
+
+	public void onClickResume(GameObject pauseMn){
+		pauseMn.SetActive(false);
+		
+	}
+
+	public void onClickExit(){
+		Application.Quit();
+	}
+}
