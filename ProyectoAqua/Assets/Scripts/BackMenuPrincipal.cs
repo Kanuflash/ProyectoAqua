d51ccd812fe8a52823df7cf867b9ffe2bc92511d
Kanuflash <35506767+Kanuflash@users.noreply.github.com>
2018-09-27 08:42:50 +0200

    Pantalla Final

---
+++ ProyectoAqua/Assets/Scripts/BackMenuPrincipal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackMenuPrincipal : MonoBehaviour {
+
+	public void LoadMenuPrincipal()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
